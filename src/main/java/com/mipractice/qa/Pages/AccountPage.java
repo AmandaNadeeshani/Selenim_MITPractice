f91406bae78e39ec362687284f3d8d44ad16e340
AmandaNadeeshani <58598621+AmandaNadeeshani@users.noreply.github.com>
2024-02-26 22:15:38 +0530

    Revert "Update AccountPage.java"

--- src/main/java/com/mipractice/qa/Pages/AccountPage.java
+++ src/main/java/com/mipractice/qa/Pages/AccountPage.java
@@ -17,7 +17,6 @@ public class AccountPage {
         PageFactory.initElements(driver,this);
     }
 
-    //add a comment
     //actioon methods
     public boolean getDosplaystatusofAccountInformationOtpion(){
         boolean displayStatus = EditYourAccountInformationOption.isDisplayed();
